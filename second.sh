88583273f715d3cb699715787196fe5170eb71a4
Per Sandström <per@helloper.com>
2016-07-01 09:52:47 +0200

    Correct mirror URL

--- second.sh
+++ second.sh
@@ -5,7 +5,7 @@ mirroruuid=$(<mirroruuid)
 
 
 # Commands #
-sed -i "\$a@chromium-browser --incognito --kiosk https://badrumsbekannelser.herokuapp.com/mirrors/\${mirroruuid}" /home/pi/.config/lxsession/LXDE-pi/autostart
+sed -i "\$a@chromium-browser --incognito --kiosk https://www.badrumsbekannelser.se/mirrors/\${mirroruuid}" /home/pi/.config/lxsession/LXDE-pi/autostart
 
 sed -i 's/width=100/width=0/g' /home/pi/.config/lxpanel/LXDE-pi/panels/panel
 sed -i 's/autohide=0/autohide=1/g' /home/pi/.config/lxpanel/LXDE-pi/panels/panel
